9155ea6b2a4f547ef7b8c2940449e8c66a8b0e60
icyfillup <phil10tran@gmail.com>
2018-02-02 23:39:57 -0800

    code for red image filter

--- CS4551/src/setupImageProject/CS4551_Main.java
+++ CS4551/src/setupImageProject/CS4551_Main.java
@@ -22,10 +22,29 @@ public class CS4551_Main
 
     // Create an Image object with the input PPM file name.
     // Display it and write it into another PPM file.
-    Image img = new Image(args[0]);
-    img.display();
-    img.write2PPM("Image/out.ppm");
+    Image img_ = new Image(args[0]);
+    for(int y = 0; y < img_.getH(); y++) 
+    {
+    	for(int x = 0; x < img_.getW(); x++) 
+        {
+    		img_.displayPixelValue(x, y);
+    		int[] rgb = new int[3];
+    		img_.getPixel(x, y, rgb);
+    		
+    		//rgb[0] = 0;
+    		rgb[1] = 0;
+    		rgb[2] = 0;
+    		
+    		img_.setPixel(x, y, rgb);
+        }
+    	if(y > img_.getH()/2)
+    		break;
+    }
+    img_.write2PPM("src/setupImageProject/image/out.ppm");
 
+    Image img = new Image("src/setupImageProject/image/out.ppm");
+    img.display();
+    
     System.out.println("--Good Bye--");
   }
 

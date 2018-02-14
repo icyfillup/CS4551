1dccff593df3b9914b89b8c3830763deac1a00b8
icyfillup <phil10tran@gmail.com>
2018-02-13 19:01:56 -0800

    finish with gray scale conversion. still need to debug

--- CS4551/src/Homework_1/CS4551_Tran.java
+++ CS4551/src/Homework_1/CS4551_Tran.java
@@ -2,10 +2,12 @@ package Homework_1;
 
 import java.util.Scanner;
 
+import setupImageProject.Image;
+
 public class CS4551_Tran
 {
 
-	public static boolean MainMain(Scanner input) 
+	public static boolean MainMain(Scanner input, Image image) 
 	{
 		System.out.println("Main Menu-----------------------------------\r\n" + 
 				"1. Conversion to Gray - scale Image (24bits -> 8bits)\r\n" + 
@@ -24,7 +26,7 @@ public class CS4551_Tran
 			case "1":
 			{
 				System.out.println("Conversion to Gray - scale Image (24bits -> 8bits):");
-				
+				GrayScaleConversion(image);
 				System.out.println();
 				isRunning = true;
 			}break;
@@ -70,11 +72,37 @@ public class CS4551_Tran
 		return isRunning;
 	}
 	
+	private static void GrayScaleConversion(Image image)
+	{
+		for(int y = 0; y < image.getH(); y++) 
+		{
+			for(int x = 0; x < image.getW(); x++) 
+			{
+				int[] rgb = new int[3];
+				image.getPixel(x, y, rgb);
+				
+				int Gray = (int) Math.round(0.299 * rgb[0] + 0.587 * rgb[1] + 0.114 * rgb[2]);
+				rgb[0] = Gray;
+				rgb[1] = Gray;
+				rgb[2] = Gray;
+				
+				image.setPixel(x, y, rgb);
+			}
+		}
+		
+		image.write2PPM("src\\Homework_1\\image\\out.ppm");
+
+	    Image img = new Image("src\\Homework_1\\image\\out.ppm");
+	    img.display();
+	}
+
 	public static void main(String[] args)
 	{
 		Scanner input = new Scanner(System.in);
+		System.out.println(System.getProperty("user.dir"));
+		Image image = new Image("src\\Homework_1\\image\\Ducky.ppm");
 		
-		while(MainMain(input)) {}
+		while(MainMain(input, image)) {}
 
 		input.close();
 	}

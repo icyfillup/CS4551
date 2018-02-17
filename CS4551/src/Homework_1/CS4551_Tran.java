2837cd9b946c13d86a79a0dd3fc2be871fb26364
icyfillup <phil10tran@gmail.com>
2018-02-17 15:27:05 -0800

    finish with error diffusion (second option) but there is artifact. if i have more time, ill debug it. started working on uniform Color Quantization.

--- CS4551/src/Homework_1/CS4551_Tran.java
+++ CS4551/src/Homework_1/CS4551_Tran.java
@@ -3,6 +3,8 @@ package Homework_1;
 import java.awt.BorderLayout;
 import java.awt.image.BufferedImage;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 import javax.swing.ImageIcon;
@@ -24,8 +26,8 @@ public class CS4551_Tran
 				"5. Quit");
 		
 		System.out.print("Please enter the task number [1 - 5]: ");
-		//String option = input.next();
-		String option = "2";
+//		String option = input.next();
+		String option = "3";
 		System.out.println();
 		
 		boolean isRunning = true;
@@ -49,27 +51,27 @@ public class CS4551_Tran
 				System.out.println("Conversion to N-level Image:");
 				String nLevelS = null;
 				
-//				boolean isAInput = false;
-//				do 
-//				{
-//					System.out.print("Please enter the N-level [2 (1 bit), 4 (2 bit), 8 (3 bit), 16 (4 bit)]: ");
-//					nLevelS = input.next();
-//					
-//					if(nLevelS.equals("2") || nLevelS.equals("4") || 
-//							nLevelS.equals("8") || nLevelS.equals("16"))
-//					{
-//						isAInput = true;
-//					}
-//				}
-//				while(!isAInput);
+				boolean isAInput = false;
+				do 
+				{
+					System.out.print("Please enter the N-level [2 (1 bit), 4 (2 bit), 8 (3 bit), 16 (4 bit)]: ");
+					nLevelS = input.next();
+					
+					if(nLevelS.equals("2") || nLevelS.equals("4") || 
+							nLevelS.equals("8") || nLevelS.equals("16"))
+					{
+						isAInput = true;
+					}
+				}
+				while(!isAInput);
 				
-//				int nLevel = Integer.parseInt(nLevelS);
-				int nLevel = Integer.parseInt("4");
+				int nLevel = Integer.parseInt(nLevelS);
+//				int nLevel = Integer.parseInt("4");
 				Image NLevelImage = NLevelConversion(image, nLevel);
 				Image NLevelErrorDiffusionImage = NLevelErrorDiffusionConversion(image, nLevel);
 
-//				NLevelImage.write2PPM("src\\Homework_1\\image\\Output1.ppm");
-//				NLevelImage.display();
+				NLevelImage.write2PPM("src\\Homework_1\\image\\Output1.ppm");
+				NLevelImage.display();
 				
 				NLevelErrorDiffusionImage.write2PPM("src\\Homework_1\\image\\Output2.ppm");
 				NLevelErrorDiffusionImage.display();
@@ -81,7 +83,7 @@ public class CS4551_Tran
 			case "3":
 			{
 				System.out.println("Conversion to 8-bit Indexed Color Image using Uniform Color Quantization(24bits -> 8bits):");
-				
+				UniformColorQuantization(image);
 				System.out.println();
 				isRunning = true;
 			}break;
@@ -111,20 +113,46 @@ public class CS4551_Tran
 		return isRunning;
 	}
 	
+	private static void UniformColorQuantization(Image image)
+	{
+		final int BIT_VALUE = 256;
+		
+		System.out.println("index\tred\tgreen\tblue");
+		System.out.println("----------------------------");
+		Map<Integer, int[]> lookUpTable = new HashMap<>();
+		
+		for(int index = 0; index < BIT_VALUE; index++) 
+		{
+			int redIndex = (0xff & (index & 224)) >> 5;
+			int greenIndex = (0xff & (index & 28)) >> 2;
+			int blueIndex = (0xff & (index & 3));
+			
+			int red = (redIndex * 32) + 16;
+			int green = (greenIndex * 32) + 16;
+			int blue = (blueIndex * 64) + 32;
+		
+			lookUpTable.put(index, new int[] {red, green, blue});
+			
+			System.out.println(index + "\t" + red + "\t" + green + "\t" + blue);
+		}
+		System.out.print("");
+			
+	}
+
 	private static Image NLevelErrorDiffusionConversion(Image image, int nLevel)
 	{
-		nLevel = 2;
+//		nLevel = 2;
 		
 		final int BIT_VALUE = 256;
-		final int GRAY_VALUE = BIT_VALUE - 1;
+		final double GRAY_VALUE = BIT_VALUE - 1;
 		double grayInterval = GRAY_VALUE / (nLevel - 1.0);
 		double grayCount = 0;
-		ArrayList<Integer> grayValues = new ArrayList<>();
-		grayValues.add((int)grayCount);
+		ArrayList<Double> grayValues = new ArrayList<>();
+		grayValues.add(grayCount);
 		while(grayCount < GRAY_VALUE) 
 		{
 			grayCount = grayCount + grayInterval;
-			grayValues.add((int)Math.floor(grayCount));
+			grayValues.add(grayCount);
 		}
 		
 		Image grayScaleImage = GrayScaleConversion(image);
@@ -137,27 +165,27 @@ public class CS4551_Tran
 				int[] rgb = new int[3];
 				grayScaleImage.getPixel(x, y, rgb);
 
-				int newGrayValue = rgb[0];
+				double grayValue = rgb[0];
 				
 				for(int i = 1; i < grayValues.size(); i++) 
 				{
 					if(rgb[0] < grayValues.get(i)) 
 					{
-						int highGrayValue = grayValues.get(i);
-						int lowGrayValue = grayValues.get(i - 1);
+						double highGrayValue = grayValues.get(i);
+						double lowGrayValue = grayValues.get(i - 1);
 						
-						int lowBound = Math.abs(lowGrayValue - rgb[0]);
-						int highBound = Math.abs(highGrayValue - rgb[0]);
+						double lowBound = Math.abs(lowGrayValue - rgb[0]);
+						double highBound = Math.abs(highGrayValue - rgb[0]);
 						
 						// the Bounds that has the lower number means that the gray pixal value 
 						// is closer to one of the n_level values
-						if(lowBound <= highBound) 
+						if(lowBound < highBound) 
 						{
-							newGrayValue = lowGrayValue;
+							grayValue = lowGrayValue;
 						}
 						else 
 						{
-							newGrayValue = highGrayValue;
+							grayValue = highGrayValue;
 						}
 						
 						break;
@@ -173,17 +201,18 @@ public class CS4551_Tran
 //					newGrayValue = 255;
 //				}
 				
-				NLevelImage.setPixel(x, y, rgb);
+				int newGrayValue = (int) Math.round(grayValue);
+				int[] newRGB = new int[] {newGrayValue, newGrayValue, newGrayValue};
+				NLevelImage.setPixel(x, y, newRGB);
 				
-				int error = rgb[0] - newGrayValue;
+				double error = (rgb[0] - grayValue);
 				
 				//right pixel
 				int xOffset = (x + 1);
 				int yOffset = y;
 				if(xOffset < grayScaleImage.getW()) 
 				{
-					double weight = 7.0 / 16.0;
-					filterWeight(grayScaleImage, xOffset, yOffset, error, weight);
+					filterWeight(grayScaleImage, xOffset, yOffset, error, 7.0 , 16.0);
 				}
 				
 				//right diagonal pixel
@@ -191,8 +220,7 @@ public class CS4551_Tran
 				yOffset = (y + 1);
 				if(xOffset < grayScaleImage.getW() && yOffset < grayScaleImage.getH()) 
 				{
-					double weight = 1.0 / 16.0;
-					filterWeight(grayScaleImage, xOffset, yOffset, error, weight);
+					filterWeight(grayScaleImage, xOffset, yOffset, error, 1.0 , 16.0);
 				}
 				
 				//bottom pixel
@@ -200,8 +228,7 @@ public class CS4551_Tran
 				yOffset = (y + 1);
 				if(yOffset < grayScaleImage.getH()) 
 				{
-					double weight = 5.0 / 16.0;
-					filterWeight(grayScaleImage, xOffset, yOffset, error, weight);
+					filterWeight(grayScaleImage, xOffset, yOffset, error, 5.0 , 16.0);
 				}
 				
 				//left diagonal pixel
@@ -209,22 +236,20 @@ public class CS4551_Tran
 				yOffset = (y + 1);
 				if(xOffset >= 0 && xOffset < grayScaleImage.getW() && yOffset < grayScaleImage.getH()) 
 				{
-					double weight = 3.0 / 16.0;
-					filterWeight(grayScaleImage, xOffset, yOffset, error, weight);
+					filterWeight(grayScaleImage, xOffset, yOffset, error, 3.0 , 16.0);
 				}
-				
 			}
 		}
 		
-		return grayScaleImage;
+		return NLevelImage;
 	}
 
-	private static void filterWeight(Image grayScaleImage, int xOffset, int yOffset, int error, double weight)
+	private static void filterWeight(Image grayScaleImage, int xOffset, int yOffset, double error, double num, double dem)
 	{
 		int[] newRGB = new int[3];
 		grayScaleImage.getPixel(xOffset, yOffset, newRGB);
 		
-		double errorDiffusionD = newRGB[0] + (error * weight);
+		double errorDiffusionD = newRGB[0] + (error * (num / dem));
 		int errorDiffusion = (int) Math.round(errorDiffusionD);
 		
 		newRGB[0] = errorDiffusion;
@@ -297,8 +322,7 @@ public class CS4551_Tran
 		
 		return NLevelImage;
 	}
-
-	// what makes this 8 bit gray scale image?  
+  
 	private static Image GrayScaleConversion(Image image)
 	{
 		Image newImage = new Image(image.getW(), image.getH());
@@ -310,8 +334,19 @@ public class CS4551_Tran
 				int[] rgb = new int[3];
 				image.getPixel(x, y, rgb);
 				
-				int Gray = (int) Math.round(0.299 * rgb[0] + 0.587 * rgb[1] + 0.114 * rgb[2]);
-
+				double GrayD = Math.round(0.299 * rgb[0] + 0.587 * rgb[1] + 0.114 * rgb[2]);
+				int Gray = (int) GrayD;
+				
+				if(Gray > 255)
+				{
+					Gray = 255;
+				}
+				
+				if(Gray < 0)
+				{
+					Gray = 0;
+				}
+				
 				rgb[0] = Gray;
 				rgb[1] = Gray;
 				rgb[2] = Gray;
@@ -327,9 +362,9 @@ public class CS4551_Tran
 	{
 		Scanner input = new Scanner(System.in);
 		System.out.println(System.getProperty("user.dir"));
-		//Image image = new Image("src\\Homework_1\\image\\Ducky.ppm");
+		Image image = new Image("src\\Homework_1\\image\\Ducky.ppm");
 		//Image image = new Image("src\\Homework_1\\image\\Buildings.ppm");
-		Image image = new Image("src\\Homework_1\\image\\Citynight.ppm");
+//		Image image = new Image("src\\Homework_1\\image\\Citynight.ppm");
 		
 		while(MainMain(input, image)) {}
 

33e7a00f458ad4999d05aaf6ac00cbdb5c51d530
icyfillup <phil10tran@gmail.com>
2018-02-23 19:51:45 -0800

    added testspace for writing test code

---
+++ CS4551/src/testspace/testbench.java
@@ -0,0 +1,26 @@
+package testspace;
+
+class Node
+{
+	String key;
+	int value;
+	
+	Node parent;
+	Node left;
+	Node right;
+}
+
+public class testbench
+{
+	Node root = null;
+	public static void main(String[] args)
+	{
+		String stream = "AADCCDD";
+		
+		for(int i = 0; i < stream.length(); i++) 
+		{
+			char element = stream.charAt(i);
+		}
+	}
+
+}

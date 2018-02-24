a4f5b2c21b1704b232043db82bdfc023df4a4695
icyfillup <phil10tran@gmail.com>
2018-02-24 14:47:57 -0800

    working on binary tree for huffmon

--- CS4551/src/testspace/testbench.java
+++ CS4551/src/testspace/testbench.java
@@ -1,25 +1,56 @@
 package testspace;
 
+import java.util.HashMap;
+import java.util.Map;
+
 class Node
 {
-	String key;
 	int value;
 	
 	Node parent;
 	Node left;
 	Node right;
+	
+	public Node() {}
+	
+	public Node(int value)
+	{
+		this.value = value;
+	}
+	
+	public Node(char key, int value) 
+	{
+		this.value = value;
+	}
+}
+
+class AdaptiveHuffmanCoding
+{
+	Node root;
+	
+	Map<Character, Node> leafNodes = new HashMap<>();
+	
+	public AdaptiveHuffmanCoding() {} 
+	
+	public void add(Node node, char element) 
+	{
+		
+	}
 }
 
 public class testbench
 {
-	Node root = null;
 	public static void main(String[] args)
 	{
 		String stream = "AADCCDD";
 		
+		AdaptiveHuffmanCoding ahc = new AdaptiveHuffmanCoding();
+		
 		for(int i = 0; i < stream.length(); i++) 
 		{
 			char element = stream.charAt(i);
+			
+			ahc.add(ahc.root, element);
 		}
 	}
 
